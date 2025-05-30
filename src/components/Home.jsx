19762a14d24b664a51f0ab59c50fe71b1e16fdf1
Khushal Solanki <121706152+Khushal1709@users.noreply.github.com>
2025-05-30 17:54:34 +0530

    map added in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -727,8 +727,17 @@ const AgricultureSection = () => {
           </div>
 
           {/* Map Section */}
-          <div className="md:w-1/2 w-full">
-            <LazyLoadImage src={map} alt="Company Location Map" className="w-full h-[300px] md:h-full object-cover rounded-lg" />
+            <div className="md:w-1/2 w-full">
+            <div className="w-full h-64 md:h-full">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3718.821663228372!2d72.8775692758764!3d21.238919280526044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04f705dffe915%3A0x1fc83e2ebcf890f5!2sTulsi%20Arcade!5e0!3m2!1sen!2sin!4v1748606234142!5m2!1sen!2sin"
+                className="w-full h-full rounded"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
           </div>
         </div>
 
